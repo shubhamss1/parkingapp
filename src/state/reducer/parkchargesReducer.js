66f5a88da3166ce646ef3c7f50d02c5cfb2e1ce3
pickupbiz <43946129+pickupbiz@users.noreply.github.com>
2023-02-21 11:16:27 +0530

    Updated

--- src/state/reducer/parkchargesReducer.js
+++ src/state/reducer/parkchargesReducer.js
@@ -1,9 +1,17 @@
-export const parkchargesReducer = (state = { parkcharges: 7 }, action) => {
+export const parkchargesReducer = (
+  state = { parkcharges: 7, model: "BMW" },
+  action
+) => {
   switch (action.type) {
     case "parkcharges":
       return {
         ...state,
         parkcharges: action.payload,
+      };
+    case "model":
+      return {
+        ...state,
+        model: action.payload,
       };
     default:
       return state;
